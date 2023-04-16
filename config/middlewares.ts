fffa06733841d5d0769156f5b6341aae99bc6de7
Anton Loginov <loginovanton95@gmail.com>
2023-04-17 00:02:42 +0200

    aws - s3 implementation

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,31 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'ah-strapi-aws-s3-media-bucket.s3.eu-central-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'ah-strapi-aws-s3-media-bucket.s3.eu-central-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
